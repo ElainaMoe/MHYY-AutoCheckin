37ee3f1aa73f661059ebfe25663f1ce61cb727ba
GamerNoTitle <GamerNoTitle@outlook.com>
2022-04-19 19:43:32 +0800

    Added Analytics

--- main.py
+++ main.py
@@ -6,20 +6,24 @@ import os
 #     config = json.loads(f.read())
 #     f.close()
 
+
 class RunError():
     pass
+
 
 # Running in Github Action, use this to get the config
 config = os.environ.get['config']
 
 token = config['token']
 client_type = config['type']
+version = config['version']
 android = config['android']
 deviceid = config['deviceid']
 devicename = config['devicename']
 devicemodel = config['devicemodel']
 appid = config['appid']
-version = '2.2.0'
+analytics = config['analytics']
+
 
 SignURL = 'https://api-cloudgame.mihoyo.com/hk4e_cg_cn/gamer/api/listNotifications?status=NotificationStatusUnread&type=NotificationTypePopup&is_sort=true'
 headers = {
@@ -40,11 +44,19 @@ headers = {
 }
 
 if __name__ == '__main__':
-    if config == '': raise RunError(f"请在Settings->Secrets->Actions页面中新建名为config的变量，并将你的配置填入后再运行！")    # Verify config
+    if config == '':
+        # Verify config
+        raise RunError(
+            f"请在Settings->Secrets->Actions页面中新建名为config的变量，并将你的配置填入后再运行！")
     else:
         if token == '' or android == 0 or deviceid == '' or devicemodel == '' or appid == 0:
             raise RunError(f'请确认您的配置文件配置正确再运行本程序！')
+    if analytics:
+        r.get(
+            f'https://analytics.api.ninym.top/mhyy?type={client_type}&version={version}&android={android}&deviceid={deviceid}&devicename={devicename}&devicemodel={devicemodel}&appid={appid}')
     res = r.get(SignURL, headers=headers)
     if json.loads(res.text)['message'] != 'OK':
-        raise RunError(f"请带着一下内容到 https://github.com/ElainaMoe/MHYY-AutoCheckin/issues 发起issue解决（或者自行解决）。签到出错，返回信息如下：{res['text']}")
-    else: pass
+        raise RunError(
+            f"请带着一下内容到 https://github.com/ElainaMoe/MHYY-AutoCheckin/issues 发起issue解决（或者自行解决）。签到出错，返回信息如下：{res['text']}")
+    else:
+        pass
